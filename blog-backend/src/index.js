a8922f5c3fd5f4c3b8433715772bd84f83058df6
min33sky <min33sky@naver.com>
2018-10-11 23:33:49 +0900

    first commit

---
+++ blog-backend/src/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const morgan = require('koa-morgan');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const api = require('./api');
+
+const { PORT: port = 4000, MONGO_URI: mongoURI } = process.env;
+
+mongoose.Promise = global.Promise; // Node의 Promise를 사용한다.
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log('connect to mongodb');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+// 로그 미들웨어 적용
+app.use(morgan('dev'));
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용전에 bodyParser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
