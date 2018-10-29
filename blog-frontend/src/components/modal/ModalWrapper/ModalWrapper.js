e5789cb5681cfe92a0db58323897f4f465c04fcc
min33sky <min33sky@naver.com>
2018-10-30 03:43:37 +0900

    apply modal-animation

--- blog-frontend/src/components/modal/ModalWrapper/ModalWrapper.js
+++ blog-frontend/src/components/modal/ModalWrapper/ModalWrapper.js
@@ -11,14 +11,48 @@ const cx = classNames.bind(styles);
  * @extends {Component}
  */
 class ModalWrapper extends Component {
+  state = {
+    animate: false
+  };
+
+  /**
+   * 애니메이션 시작
+   *
+   * @memberof ModalWrapper
+   */
+  startAnimation = () => {
+    // animate 값을 true로 설정 후
+    this.setState({
+      animate: true
+    });
+    // 250ms 이후 다시 false로 설정
+    setTimeout(() => {
+      this.setState({
+        animate: false
+      });
+    }, 250);
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.visible !== this.props.visible) {
+      this.startAnimation();
+    }
+  }
+
   render() {
     const { children, visible } = this.props;
-    if (!visible) return null; // 모달을 보여주지 않는다.
+    const { animate } = this.state;
+
+    // 모달을 보여주지 않는다.
+    if (!visible && !animate) return null;
+
+    // 상태에 따라 애니메이션 설정
+    const animation = animate && (visible ? 'enter' : 'leave');
 
     return (
-      <div className={cx('gray-background')}>
+      <div className={cx('gray-background', animation)}>
         <div className={cx('modal-wrapper')}>
-          <div className={cx('modal')}>{children}</div>
+          <div className={cx('modal', animation)}>{children}</div>
         </div>
       </div>
     );
